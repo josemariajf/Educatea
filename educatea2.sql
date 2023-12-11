56bbfb405cd0d03a6d0085c3e5dbeb489b0b89ff
josemariajf <117285851+josemariajf@users.noreply.github.com>
2023-12-11 16:47:51 +0100

    Add files via upload

--- educatea2.sql
+++ educatea2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2023 a las 22:59:10
+-- Tiempo de generación: 11-12-2023 a las 16:47:41
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -33,6 +33,15 @@ CREATE TABLE `asignaturas` (
   `codigo_asignatura` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `asignaturas`
+--
+
+INSERT INTO `asignaturas` (`asignatura_id`, `nombre_asignatura`, `codigo_asignatura`) VALUES
+(14, 'Matematicas', 'MTM'),
+(15, 'Lengua Castellana', 'LG'),
+(16, 'Educacion fisica', 'EF');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +54,13 @@ CREATE TABLE `asignaturas_clases` (
   `clase_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `asignaturas_clases`
+--
+
+INSERT INTO `asignaturas_clases` (`asignatura_clase_id`, `asignatura_id`, `clase_id`) VALUES
+(34, 14, 30);
+
 -- --------------------------------------------------------
 
 --
@@ -54,8 +70,38 @@ CREATE TABLE `asignaturas_clases` (
 CREATE TABLE `clases` (
   `clase_id` int(11) NOT NULL,
   `nombre_clase` varchar(100) NOT NULL,
-  `curso` varchar(50) NOT NULL
+  `curso` varchar(50) NOT NULL,
+  `id_tutor` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clases`
+--
+
+INSERT INTO `clases` (`clase_id`, `nombre_clase`, `curso`, `id_tutor`) VALUES
+(30, '1ªA', '2023/2024', 3),
+(31, '2ªA', '2023/2024', 0),
+(32, '3ªA', '2023/2024', 0),
+(33, '4ªA', '2023/2024', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clases_usuarios`
+--
+
+CREATE TABLE `clases_usuarios` (
+  `id` int(11) NOT NULL,
+  `clase_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clases_usuarios`
+--
+
+INSERT INTO `clases_usuarios` (`id`, `clase_id`, `usuario_id`) VALUES
+(33, 30, 1);
 
 -- --------------------------------------------------------
 
@@ -95,6 +141,21 @@ CREATE TABLE `tareas` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tareas_usuarios`
+--
+
+CREATE TABLE `tareas_usuarios` (
+  `id` int(11) NOT NULL,
+  `tarea_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `calificacion` double DEFAULT NULL,
+  `url` varchar(255) NOT NULL,
+  `fecha_entrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -113,20 +174,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`usuario_id`, `usuario`, `nombre`, `apellido`, `correo_electronico`, `contrasena`, `rol_id`) VALUES
+(0, 'nulo', 'nulo', 'nulo', 'nulo@gmail.com', 'nulo', 3),
 (1, 'alumno', 'alumno', 'alumno', 'alumno@gmail.com', 'c6865cf98b133f1f3de596a4a2894630', 1),
 (3, 'profesor', 'profesor', 'profesor', 'profesor@gmail.com', '793741d54b00253006453742ad4ed534', 2),
 (4, 'director', 'director', 'director', 'director@gmail.com', '3d4e992d8d8a7d848724aa26ed7f4176', 3);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios_asignaturas`
---
-
-CREATE TABLE `usuarios_asignaturas` (
-  `usuario_id` int(11) NOT NULL,
-  `asignatura_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
@@ -150,7 +201,16 @@ ALTER TABLE `asignaturas_clases`
 -- Indices de la tabla `clases`
 --
 ALTER TABLE `clases`
-  ADD PRIMARY KEY (`clase_id`);
+  ADD PRIMARY KEY (`clase_id`),
+  ADD KEY `fk_tutor` (`id_tutor`);
+
+--
+-- Indices de la tabla `clases_usuarios`
+--
+ALTER TABLE `clases_usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `clase_id` (`clase_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `roles`
@@ -168,19 +228,20 @@ ALTER TABLE `tareas`
   ADD KEY `asignatura_id` (`asignatura_id`);
 
 --
+-- Indices de la tabla `tareas_usuarios`
+--
+ALTER TABLE `tareas_usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tarea_id` (`tarea_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`usuario_id`),
   ADD UNIQUE KEY `correo_electronico` (`correo_electronico`),
   ADD KEY `rol_id` (`rol_id`);
-
---
--- Indices de la tabla `usuarios_asignaturas`
---
-ALTER TABLE `usuarios_asignaturas`
-  ADD PRIMARY KEY (`usuario_id`,`asignatura_id`),
-  ADD KEY `asignatura_id` (`asignatura_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -190,19 +251,25 @@ ALTER TABLE `usuarios_asignaturas`
 -- AUTO_INCREMENT de la tabla `asignaturas`
 --
 ALTER TABLE `asignaturas`
-  MODIFY `asignatura_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `asignatura_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `asignaturas_clases`
 --
 ALTER TABLE `asignaturas_clases`
-  MODIFY `asignatura_clase_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `asignatura_clase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `clases`
 --
 ALTER TABLE `clases`
-  MODIFY `clase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `clase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `clases_usuarios`
+--
+ALTER TABLE `clases_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -214,13 +281,19 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `tarea_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tarea_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de la tabla `tareas_usuarios`
+--
+ALTER TABLE `tareas_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
@@ -234,6 +307,19 @@ ALTER TABLE `asignaturas_clases`
   ADD CONSTRAINT `asignaturas_clases_ibfk_2` FOREIGN KEY (`clase_id`) REFERENCES `clases` (`clase_id`);
 
 --
+-- Filtros para la tabla `clases`
+--
+ALTER TABLE `clases`
+  ADD CONSTRAINT `fk_tutor` FOREIGN KEY (`id_tutor`) REFERENCES `usuarios` (`usuario_id`);
+
+--
+-- Filtros para la tabla `clases_usuarios`
+--
+ALTER TABLE `clases_usuarios`
+  ADD CONSTRAINT `clases_usuarios_ibfk_1` FOREIGN KEY (`clase_id`) REFERENCES `clases` (`clase_id`),
+  ADD CONSTRAINT `clases_usuarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
 -- Filtros para la tabla `tareas`
 --
 ALTER TABLE `tareas`
@@ -242,17 +328,17 @@ ALTER TABLE `tareas`
   ADD CONSTRAINT `tareas_ibfk_3` FOREIGN KEY (`asignatura_id`) REFERENCES `asignaturas` (`asignatura_id`);
 
 --
+-- Filtros para la tabla `tareas_usuarios`
+--
+ALTER TABLE `tareas_usuarios`
+  ADD CONSTRAINT `tareas_usuarios_ibfk_1` FOREIGN KEY (`tarea_id`) REFERENCES `tareas` (`tarea_id`),
+  ADD CONSTRAINT `tareas_usuarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`);
+
+--
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`rol_id`);
-
---
--- Filtros para la tabla `usuarios_asignaturas`
---
-ALTER TABLE `usuarios_asignaturas`
-  ADD CONSTRAINT `usuarios_asignaturas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`),
-  ADD CONSTRAINT `usuarios_asignaturas_ibfk_2` FOREIGN KEY (`asignatura_id`) REFERENCES `asignaturas` (`asignatura_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
